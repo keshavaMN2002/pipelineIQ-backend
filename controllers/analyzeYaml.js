9d5c4d78a0603d9030801621f783d46e9287a579
Channabasava Halemani <channabasavahalemani49@gmail.com>
2026-04-24 18:53:01 +0530

    integrated ai model

--- controllers/analyzeYaml.js
+++ controllers/analyzeYaml.js
@@ -1,11 +1,25 @@
 import yaml from "js-yaml";
-import pkg from 'deep-diff';
+import pkg from "deep-diff";
 const { diff } = pkg;
 import { rules } from "../utils/rules.js";
-import { estimateTime, calculateScore, optimizeYaml } from "../utils/timeCalculator.js";
+import {
+  estimateTime,
+  calculateScore,
+  optimizeYaml,
+} from "../utils/timeCalculator.js";
 import { detectExecutionFlow } from "../utils/execution.js";
+import OpenAI from "openai";
 
-export const analyzeYaml = (req, res) => {
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey:
+    process.env.PIPELINE_KEY ||
+    "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",
+});
+
+console.log(process.env.PIPELINE_KEY);
+
+export const analyzeYaml = async (req, res) => {
   try {
     const { yamlContent } = req.body;
 
@@ -14,7 +28,51 @@ export const analyzeYaml = (req, res) => {
     }
 
     // Parse
-    const parsed = yaml.load(yamlContent);
+    let parsed;
+    try {
+      parsed = yaml.load(yamlContent);
+    } catch (e) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid YAML format",
+      });
+    }
+
+    let ai_suggestions;
+    try {
+      const completion = await openai.chat.completions.create({
+      model: "meta-llama/llama-3.1-8b-instruct",
+      max_tokens: 1024,
+      messages: [
+        {
+          role: "system",
+          content:
+            `
+You are a CI/CD expert.
+
+Analyze the given YAML and return EXACTLY 5 suggestions.
+
+Rules:
+- Max 10 words each
+- No explanations
+- No numbering
+- Return as plain lines only
+`,
+        },
+        {
+          role: "user",
+          content: `Analyze this CI/CD YAML and suggest optimizations:\n\n${yamlContent}`,
+        },
+      ],
+    });
+
+    ai_suggestions = completion.choices[0]?.message?.content;
+    } catch (error) {
+      console.log("Error: ", error)
+    }
+
+    console.log(ai_suggestions)
+   
 
     // Suggestions
     const suggestions = rules
@@ -47,12 +105,12 @@ export const analyzeYaml = (req, res) => {
       parsed,
       estimatedTime,
       suggestions,
+      ai_suggestions,
       execution,
       score,
       optimizedYaml,
       diff: differences,
     });
-
   } catch (err) {
     return res.status(400).json({
       success: false,
